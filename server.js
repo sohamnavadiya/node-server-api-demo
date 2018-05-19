f37510f4b99c7102a88756d32830370d4eba5807
soham <som.insomniacs@gmail.com>
2018-05-19 21:05:25 +0530

    test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const hbs = require('hbs');
 var MongoClient = require('mongodb').MongoClient;
 
 var uri = "mongodb+srv://mongodb-stitch-sample-ipjjx:7gShZY30dHAhodZ0@cluster0-cdzyn.mongodb.net/admin";
@@ -8,17 +7,10 @@ var app = express();
 var randomstring = require("randomstring");
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.render('home.hbs', {
-        pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my website',
-        currentYear: new Date().getFullYear()
-    })
-});
 
 app.get('/about', (req, res) => {
+    
     
     MongoClient.connect(uri, function(err, db){
         const a = randomstring.generate(7);
@@ -46,19 +38,6 @@ app.get('/about', (req, res) => {
    
 });
 
-// app.get('/about', (req, res) => {
-//     res.render('about.hbs', {
-//         pageTitle: 'About Page',
-//         currentYear: new Date().getFullYear()
-//     })
-//     res.send('About Page');
-// });
-
-app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'Unable to handle request'  
-    })
-});
 
 app.listen(3000, () => {
     console.log('server is up and running...port 3000');
